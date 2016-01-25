d8507dc74762d19e1175108e56422665d92b72ec
JosiahDub <josiahwhite@gmail.com>
2016-01-24 20:36:35 -0500

    These need committing for some reason.

--- josiah/test_script.py
+++ josiah/test_script.py
@@ -1,9 +1,7 @@
 __author__ = 'josiah'
 from josiah.MineSolver import MineSolver
 
-# (6, 1) will not reveal final unrevealed
-
 mine = MineSolver()
 blank_block = mine.servant.get_random_blank_block()
 mine.reveal_squares([blank_block])
-mine.solver()
+mine.logical_solver()
