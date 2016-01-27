7999b4c91651ea1cda9f43803355f33d47f949a2
JosiahDub <josiahwhite@gmail.com>
2016-01-27 07:23:26 -0500

    Got rid of self.field in favor of set operations. Trying to track down an algorithm error that assigning incorrect flags.

--- josiah/MineSemiSolver.py
+++ josiah/MineSemiSolver.py
@@ -5,27 +5,20 @@ from random import choice
 
 class MineSemiSolver:
     def __init__(self, unrevealed, number_dictionary, rows, columns):
+        self.unrevealed = unrevealed
         self.number_dictionary = number_dictionary
         self.rows = rows
         self.columns = columns
         self.flags = []
         self.field = [[0 for column in range(0, self.columns)]
                       for row in range(0, self.rows)]
-        for coordinate in self.number_dictionary.keys():
-            self.field[coordinate[0]][coordinate[1]] = self.number_dictionary[coordinate]
-        for coordinate in unrevealed:
-            self.field[coordinate[0]][coordinate[1]] = -1
         self.unchecked_blocks = self.number_dictionary.keys()
         self.blocks_to_remove = set([])
         self.possible_plays = []
 
-    def reveal_squares(self, coordinate):
-        # self.unrevealed.remove(coordinate)
-        self.field[coordinate[0]][coordinate[1]] = 0
-
     def plant_flag(self, coordinate):
         self.flags.append(coordinate)
-        self.field[coordinate[0]][coordinate[1]] = 0
+        self.unrevealed.remove(coordinate)
 
     def remove_checked_blocks(self):
         """
@@ -38,19 +31,24 @@ class MineSemiSolver:
 
     def choose(self):
         for coordinate in self.unchecked_blocks:
+            print '*****COORDINATE*****', coordinate
             real_block_value = self.get_real_block_value(coordinate)
+            print 'real block value: ', real_block_value
             unrevealed = self.get_unrevealed_blocks(coordinate)
+            print 'unrevealed: ', unrevealed
             if real_block_value <= 0 or len(unrevealed) == 0:
                 continue
             # All possible locations of the mine(s). We need to only choose one.
-            all_sets = combinations(unrevealed, real_block_value)
+            all_sets = combinations(list(unrevealed), real_block_value)
             chosen_placement = choice(list(all_sets))
+            print 'chosen set: ', chosen_placement
             not_chosen = set(unrevealed).copy()
             for chosen in chosen_placement:
                 self.plant_flag(chosen)
                 not_chosen.remove(chosen)
             for index in not_chosen:
-                self.field[index[0]][index[1]] = 0
+                self.unrevealed.remove(index)
+            print 'global unrevealed: ', self.unrevealed
 
     def get_real_block_value(self, coordinate):
         """
@@ -58,7 +56,7 @@ class MineSemiSolver:
         :param coordinate:
         :return:
         """
-        block_value = self.field[coordinate[0]][coordinate[1]]
+        block_value = self.number_dictionary[coordinate]
         flags = self.get_num_flag_neighbors(coordinate)
         return block_value - flags
 
@@ -68,11 +66,10 @@ class MineSemiSolver:
         :param coordinate:
         :return:
         """
-        surrounding_coords = self.get_surrounding_block_coords(coordinate)
-        unrevealed_coords = []
-        for coord in surrounding_coords:
-            if self.field[coord[0]][coord[1]] == -1:
-                unrevealed_coords.append(coord)
+        # Gets all surrounding coordinates
+        unrevealed_coords = self.get_surrounding_block_coords(coordinate)
+        # Reduces to what's in self.unrevealed
+        unrevealed_coords.intersection_update(self.unrevealed)
         return unrevealed_coords
 
     def get_num_flag_neighbors(self, coordinate):
@@ -81,11 +78,12 @@ class MineSemiSolver:
         :param coordinate:
         :return:
         """
-        flag_neighbors = 0
-        surrounding_coords = set(self.get_surrounding_block_coords(coordinate))
-        for coord in surrounding_coords:
-            if coord in self.flags:
-                flag_neighbors += 1
+        # Gets all surrounding coordinates
+        flag_coords = self.get_surrounding_block_coords(coordinate)
+        # Reduces to what's in self.flags
+        flag_coords.intersection_update(self.flags)
+        # Gets the length of that
+        flag_neighbors = len(flag_coords)
         return flag_neighbors
 
     def get_surrounding_block_coords(self, coordinate):
@@ -124,4 +122,4 @@ class MineSemiSolver:
         surrounding_blocks = [(row, column)
                               for row in range(start_row, end_row + 1)
                               for column in range(start_column, end_column + 1)]
-        return surrounding_blocks
+        return set(surrounding_blocks)
